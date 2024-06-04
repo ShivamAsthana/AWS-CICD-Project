c05516b572dd4a8a9e1b29c13fd89560cd38f9c3
ShivamAsthana <86240594+ShivamAsthana@users.noreply.github.com>
2024-06-04 17:49:21 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 #docker pull sreedhar8897/hotel-app:latest
-sudo docker push shivamchamp/hotel-app:latest
+sudo docker pull shivamchamp/hotel-app:latest
 
 # Run the Docker image as a container
 #docker run -dit -p 80:80 sreedhar8897/hotel-app
